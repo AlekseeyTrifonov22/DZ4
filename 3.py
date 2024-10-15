8c2b327215ba3bacc3653b3099fa9f258c54c491
AlekseeyTrifonov22 <bobikbobik2022@mail.ru>
2024-10-15 14:58:15 +0300

    DZ3

---
+++ 3.py
@@ -0,0 +1,11 @@
+x1 = int(input("Введите число: "))
+f = int(input("1 - Мили\n2 - дюймы\n3 - ярды\n"))
+if f == 1:
+    print(x1 * 0.000621)
+elif f == 2:
+    print(x1 * 39.37)
+elif f == 3:
+    print(x1 * 1.0936)
+
+
+
